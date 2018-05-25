b769f6e10a95326d171786bbb50330a245aed096
Juansebar <juansebar1991@gmail.com>
2018-05-25 13:49:05 -0400

    Added adapter for listView and created custom ListViewItem

--- app/src/main/java/com/juanse/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/juanse/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.juanse.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.juanse.coderswag.Model.Category
 import com.juanse.coderswag.R
+import com.juanse.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Adapter is the one that manages data to present in ListView
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the Adapter providing (context, view type, data to present)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        // Tell ListView who it needs to listen to
+        categoryListView.adapter = adapter
     }
 }
