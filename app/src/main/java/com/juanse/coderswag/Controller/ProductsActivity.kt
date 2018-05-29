89d8b72f8ed07c0d70b842ff25880333689581c3
Juansebar <juansebar1991@gmail.com>
2018-05-29 13:59:25 -0400

    Implemented Product Recycler Adapter

--- app/src/main/java/com/juanse/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/juanse/coderswag/Controller/ProductsActivity.kt
@@ -1,17 +1,45 @@
 package com.juanse.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.juanse.coderswag.Adapters.ProductsAdapter
 import com.juanse.coderswag.R
+import com.juanse.coderswag.Services.DataService
 import com.juanse.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        // Setting more columns for landscape orientation
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        // Check orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        // Check screen size
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) { // Greater than 720dp is Extra Large
+            spanCount = 3
+        }
+
+        // Layout
+        val layoutManager = GridLayoutManager(this, spanCount) // spanCount -> Number of columns
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter // Set the adapter for RecycleListView
     }
 }
--- app/src/main/java/com/juanse/coderswag/Services/DataService.kt
+++ app/src/main/java/com/juanse/coderswag/Services/DataService.kt
@@ -27,6 +27,14 @@ object DataService {
             Product("Devslopes Graphic Beanie", "18", "hat1"),
             Product("Devslopes Hat Balck", "20", "hat2"),
             Product("Devslopes Hat White", "18", "hat3"),
+            Product("Devslopes Hat Snapback", "22", "hat4"),
+            Product("Devslopes Graphic Beanie", "18", "hat1"),
+            Product("Devslopes Hat Balck", "20", "hat2"),
+            Product("Devslopes Hat White", "18", "hat3"),
+            Product("Devslopes Hat Snapback", "22", "hat4"),
+            Product("Devslopes Graphic Beanie", "18", "hat1"),
+            Product("Devslopes Hat Balck", "20", "hat2"),
+            Product("Devslopes Hat White", "18", "hat3"),
             Product("Devslopes Hat Snapback", "22", "hat4")
     )
 
@@ -34,10 +42,28 @@ object DataService {
             Product("Devslopes Hoddie Gray", "28", "hoodie1"),
             Product("Devslopes Hoddie Red", "32", "hoodie2"),
             Product("Devslopes Gray Hoodie", "28", "hoodie3"),
+            Product("Devslopes Black Hoodie", "32", "hoodie4"),
+            Product("Devslopes Hoddie Gray", "28", "hoodie1"),
+            Product("Devslopes Hoddie Red", "32", "hoodie2"),
+            Product("Devslopes Gray Hoodie", "28", "hoodie3"),
+            Product("Devslopes Black Hoodie", "32", "hoodie4"),
+            Product("Devslopes Hoddie Gray", "28", "hoodie1"),
+            Product("Devslopes Hoddie Red", "32", "hoodie2"),
+            Product("Devslopes Gray Hoodie", "28", "hoodie3"),
             Product("Devslopes Black Hoodie", "32", "hoodie4")
     )
 
     val shirts = listOf(
+            Product("Devslopes Shirt Black", "18", "shirt1"),
+            Product("Devslopes Badge Light Gray", "20", "shirt2"),
+            Product("Devslopes Logo Shirt Red", "22", "shirt3"),
+            Product("Devslopes Hustle", "22", "shirt4"),
+            Product("Kickflip Studios", "18", "shirt5"),
+            Product("Devslopes Shirt Black", "18", "shirt1"),
+            Product("Devslopes Badge Light Gray", "20", "shirt2"),
+            Product("Devslopes Logo Shirt Red", "22", "shirt3"),
+            Product("Devslopes Hustle", "22", "shirt4"),
+            Product("Kickflip Studios", "18", "shirt5"),
             Product("Devslopes Shirt Black", "18", "shirt1"),
             Product("Devslopes Badge Light Gray", "20", "shirt2"),
             Product("Devslopes Logo Shirt Red", "22", "shirt3"),
@@ -51,7 +77,7 @@ object DataService {
         return when (category.toUpperCase()) {  // Switch statement
             "SHIRTS" -> shirts
             "HATS" -> hats
-            "HOODIES" -> hoodies
+            "HODDIE" -> hoodies
             else -> digitalGoods
         }
     }
